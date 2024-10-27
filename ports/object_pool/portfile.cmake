84de606b333994c24d7beaa22ab8c9701691ca9a
Andatr <malcev.alexander@gmail.com>
2024-10-27 19:57:57 +0000

    Update object-pool SHA

--- ports/object_pool/portfile.cmake
+++ ports/object_pool/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO andatr/object_pool
   REF "v${VERSION}"
-  SHA512 e4765e1367c63506c02140bca4cd0337ae999f191684f5d1fd62ea3b2effbc4fb78bc9dce12e7f4b1f0f5383107f4e6da052cb3d0492c14cb01de6d5e1d6e9fc
+  SHA512 5162ca2dc7a64f9a80b2ac3cbe4d4e4513067395ff45c05eaa875a44ca2251e85b2e93d7be1dbf741703b7f619fd1e32dbb5d726ca492e76ba4c8a5cd6f5940d
   HEAD_REF master
 )
 vcpkg_cmake_configure(
