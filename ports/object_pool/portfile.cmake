cd08bb2ca2ebe1840ddc919f1f2b3bd43fa7ba00
Andatr <malcev.alexander@gmail.com>
2024-10-27 19:52:46 +0000

    Provide additional options when building object_pool

--- ports/object_pool/portfile.cmake
+++ ports/object_pool/portfile.cmake
@@ -5,5 +5,8 @@ vcpkg_from_github(
   SHA512 e4765e1367c63506c02140bca4cd0337ae999f191684f5d1fd62ea3b2effbc4fb78bc9dce12e7f4b1f0f5383107f4e6da052cb3d0492c14cb01de6d5e1d6e9fc
   HEAD_REF master
 )
-
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS "-DOBJECT_POOL_TEST=OFF -DOBJECT_POOL_EXAMPLES=OFF"
+)
 vcpkg_cmake_install()
